c62ed27f325ace31e74bc0b93133e7ba789815fd
safranfais <safranfais@gmail.com>
2019-06-26 13:42:42 +0530

    Add files via upload

---
+++ fxDeals/src/main/java/com/bloomberg/deals/fxDeals/FxDealsApplication.java
@@ -0,0 +1,13 @@
+package com.bloomberg.deals.fxDeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxDealsApplication.class, args);
+	}
+
+}
