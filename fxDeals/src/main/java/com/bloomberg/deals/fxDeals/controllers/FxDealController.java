b75fad91929a2dd433adba5a11b3df808b3c4f7a
mfmsafran <mfmsafran@virtusa.com>
2019-07-01 17:37:14 +0530

    saving code added

--- fxDeals/src/main/java/com/bloomberg/deals/fxDeals/controllers/FxDealController.java
+++ fxDeals/src/main/java/com/bloomberg/deals/fxDeals/controllers/FxDealController.java
@@ -1,17 +1,18 @@
 package com.bloomberg.deals.fxDeals.controllers;
 
 import com.bloomberg.deals.fxDeals.models.FxDealDataWarehouseModel;
+import com.bloomberg.deals.fxDeals.repository.FxDealRepository;
 import com.bloomberg.deals.fxDeals.utils.CSVUtils;
 import com.bloomberg.deals.fxDeals.utils.Constants;
 import com.bloomberg.deals.fxDeals.utils.ValidatingModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +21,9 @@ import java.util.List;
 public class FxDealController {
 
     private static final Logger LOGGER = LogManager.getLogger(FxDealController.class);
+
+    @Autowired
+    private FxDealRepository fxDealRepository ;
 
 
     public static final String NAME = "Fx Deal File Uploading ";
@@ -33,7 +37,6 @@ public class FxDealController {
 
     @PostMapping("/upload")
     public String fxFileUpload(@RequestParam("fxfile") MultipartFile file, RedirectAttributes redirectAttributes) {
-
         if (file.isEmpty()) {
             LOGGER.debug("There is no file was uploaded!!!");
             redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
@@ -65,6 +68,9 @@ public class FxDealController {
                         invalidFxDealDataWarehouseModels.add(result);
                     }
                 }
+
+//                fxDealRepository.save(fxDealDataWarehouseModels);
+
                 bufferedReader.close();
                 redirectAttributes.addFlashAttribute("message", "Successfully uploaded Your File : [ " + file.getOriginalFilename() + "]");
             } else {
@@ -79,6 +85,7 @@ public class FxDealController {
 
         return "redirect:/uploadStatus";
     }
+
 
 
     @GetMapping("/uploadStatus")
